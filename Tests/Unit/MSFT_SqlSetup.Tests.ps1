c755bf48117c1a99189b2af96f9bbf0d805d953f
Johan Ljunggren <johan.ljunggren@shamn.se>
2018-04-08 16:08:31 +0200

    Changes to SqlSetup - Localized messages for Master Data Services no longer start and end with   single quote. - When installing features a verbose message is written if a feature is found   to already be installed. It no longer quietly removes the feature from the   `/FEATURES` argument. - Cleaned up a bit in the tests, removed excessive piping.

--- Tests/Unit/MSFT_SqlSetup.Tests.ps1
+++ Tests/Unit/MSFT_SqlSetup.Tests.ps1
@@ -3026,9 +3026,8 @@ try
                 }
             }
 
-            $testProductVersion | ForEach-Object -Process {
-                $mockSqlMajorVersion = $_
-
+            foreach ($mockSqlMajorVersion in $testProductVersion )
+            {
                 $mockDefaultInstance_InstanceId = "$($mockSqlDatabaseEngineName)$($mockSqlMajorVersion).$($mockDefaultInstance_InstanceName)"
 
                 $mockSqlInstallPath = "C:\Program Files\Microsoft SQL Server\$($mockDefaultInstance_InstanceId)\MSSQL"
@@ -4797,9 +4796,8 @@ try
                     New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'COMPANY\$sql.service', $mockServiceAccountPassword
             }
 
-            $serviceTypes | ForEach-Object {
-                $serviceType = $_
-
+            foreach ($serviceType in $serviceTypes )
+            {
                 Context "When service type is $serviceType" {
                     $mockAccountArgumentName = ('{0}SVCACCOUNT' -f $serviceType)
                     $mockPasswordArgumentName = ('{0}SVCPASSWORD' -f $serviceType)
